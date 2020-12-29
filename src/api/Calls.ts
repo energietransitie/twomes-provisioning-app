c052279e88643cf73d215f7e2647875791dc185f
Brian Hoen <lordpalf123@gmail.com>
2020-12-29 13:02:40 +0100

    added some api calls

--- src/api/Calls.ts
+++ src/api/Calls.ts
@@ -18,6 +18,10 @@ export default {
         sendHardwareID: function(token: string, id: string) {
             var url = '/';
             return DatabaseAPI.get(url);
+        },
+        checkUserID: function(token: string) {
+            var url = `/get/house/registration?token=${token}`
+            return DatabaseAPI.get(url);
         }
     },
     weather: {
