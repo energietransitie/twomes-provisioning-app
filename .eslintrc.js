d51e63696a600db239a09151574b5264b8a711aa
mk-prins <marco@kaartje2go.nl>
2021-02-23 11:44:11 +0100

    add nvmrc

---
+++ .eslintrc.js
@@ -0,0 +1,103 @@
+/* eslint-disable max-lines,quote-props */
+module.exports = {
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        },
+        requireConfigFile: false
+    },
+    plugins: [
+        'eslint-plugin-import',
+        'eslint-plugin-jest',
+        'eslint-plugin-jsdoc',
+        'eslint-plugin-jsx',
+        'eslint-plugin-react'
+    ],
+    extends: [
+        'plugin:jest/all',
+        'plugin:react-hooks/recommended'
+    ],
+    env: {
+        es6: true,
+        browser: true,
+        node: true,
+        jest: true
+    },
+    // globals: {},
+    settings: {
+        'import/parsers': {
+            'babel-eslint': [
+                '.js',
+                '.jsx'
+            ],
+            '@typescript-eslint/parser': [
+                '.ts',
+                '.tsx'
+            ]
+        },
+        'import/resolver': {
+            node: {
+                extensions: [
+                    '.tsx',
+                    '.ts',
+                    '.jsx',
+                    '.js'
+                ]
+            }
+        },
+        jsdoc: {
+            tagNamePreference: {
+                returns: 'return'
+            }
+        },
+        react: {
+            version: 'detect'
+        }
+    },
+    overrides: [
+        {
+            files: ['**/*.{ts,tsx}'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                project: './tsconfig.json'
+            },
+            plugins: [
+                '@typescript-eslint/eslint-plugin'
+            ],
+            extends: [
+                'plugin:@typescript-eslint/recommended',
+                'plugin:jest/all',
+                'plugin:react-hooks/recommended'
+            ],
+            rules: {
+                '@typescript-eslint/ban-ts-ignore': 0,
+                '@typescript-eslint/interface-name-prefix': 0,
+                '@typescript-eslint/explicit-function-return-type': 2,
+                '@typescript-eslint/no-var-requires': 2,
+                '@typescript-eslint/explicit-module-boundary-types': 2,
+                '@typescript-eslint/no-explicit-any': 2,
+                '@typescript-eslint/ban-types': 2,
+                '@typescript-eslint/no-non-null-assertion': 2,
+                'jest/prefer-expect-assertions': 0,
+                'jest/no-hooks': 0,
+                'jest/lowercase-name': 0
+            }
+        }
+    ],
+    rules: {
+        '@typescript-eslint/ban-ts-ignore': 0,
+        '@typescript-eslint/interface-name-prefix': 0,
+        '@typescript-eslint/explicit-function-return-type': 2,
+        '@typescript-eslint/no-var-requires': 2,
+        '@typescript-eslint/explicit-module-boundary-types': 2,
+        '@typescript-eslint/no-explicit-any': 2,
+        '@typescript-eslint/ban-types': 2,
+        '@typescript-eslint/no-non-null-assertion': 2,
+        'jest/prefer-expect-assertions': 0,
+        'jest/no-hooks': 0,
+        'jest/lowercase-name': 0
+    }
+};
